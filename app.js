4ab4b63526f9ab55d68004c02c5dec1f6cc385e4
SeungSeok90 <shinss305@gmail.com>
2024-06-12 05:28:27 +0900

    env add

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const path = require('path');
 const express = require('express');
 
+require('dotenv').config();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+
+
 const defaultRoutes = require('./routes/default');
 const restaurantRoutes = require('./routes/restaurants');
 
